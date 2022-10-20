2e9502815097507549246c7d4c1fb4afde5c6822
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2022-10-20 10:17:32 +0530

    Time: 91 ms (30.28%), Space: 13.9 MB (32.67%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.py
+++ 0299-bulls-and-cows/0299-bulls-and-cows.py
@@ -1,18 +1,22 @@
 class Solution:
     def getHint(self, secret: str, guess: str) -> str:
+        # Dictionary for Lookup
         lookup = Counter(secret)
         
         x, y = 0, 0
+        
+        """ The reason for using two for loop is in this problem we have to give             first priority to number which are at correct position, Therefore we are             first updating x value """
+        
+        # First finding numbers which are at correct position and updating x
         for i in range(len(guess)):
             if secret[i] == guess[i]:
                 x+=1
                 lookup[secret[i]]-=1
         
-        print(lookup)
+        # Finding numbers which are present in secret but not at correct position 
         for i in range(len(guess)):
             if guess[i] in lookup and secret[i] != guess[i] and lookup[guess[i]]>0:
                 y+=1
                 lookup[guess[i]]-=1
-        print(lookup)
         
         return "{}A{}B".format(x, y)
