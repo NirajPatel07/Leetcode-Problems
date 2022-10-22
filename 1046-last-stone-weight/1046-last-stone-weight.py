5419a54fc805a3687175dc4724dba8323a0e1ced
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2022-10-22 11:04:59 +0530

    Time: 46 ms (73.63%), Space: 14.3 MB (15.40%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,10 @@
+from sortedcontainers import SortedList
+
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        sl = SortedList(stones)
+        while len(sl) >= 2:
+            y = sl.pop()
+            x = sl.pop()
+            if y > x: sl.add(y - x)  # Note that sl is a SortedList
+        return sl.pop() if len(sl) else 0
