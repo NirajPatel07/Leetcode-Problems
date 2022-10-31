2f81ab6e9810352f846f3ea3a124a2ec4a8fa4eb
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2022-10-31 09:31:14 +0530

    Time: 195 ms (41.32%), Space: 14 MB (36.74%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.py
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -1,10 +1,16 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]])->bool:
+        # Initialise Row lenght and Column length
         r_len, c_len = len(matrix),len(matrix[0])
         
+        # For every row
         for r in range (1, r_len):
+            # For every column
             for c in range (1, c_len):
+                # Check if diagonal element are equal or not
                 if matrix[r][c]!=matrix[r-1][c-1]:
+                    # If diagonal elements are not equal then return False
                     return False
         
+        # If for loop is executed it means it satify the condition. Return True
         return True
