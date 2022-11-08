79ad60f95fbdf8eefde0b45da8ed0017c1b09254
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2022-11-08 09:36:04 +0530

    Time: 72 ms (42.86%), Space: 13.8 MB (97.60%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.py
+++ 1544-make-the-string-great/1544-make-the-string-great.py
@@ -1,11 +1,16 @@
 class Solution:
     def makeGood(self, s: str) -> str:
+        # Initialise Stack
         stack = []
         
         for i in range(len(s)):
+            # If element is present in stack and the condition is satfied 
             if stack and stack[-1] == s[i].swapcase():
+                # then remove the top letter in stack
                 stack.pop()
             else:
+                # If the Condition is not satified then append the letter
                 stack.append(s[i])
         
+        # Return letter present in stack
         return ''.join(stack)
