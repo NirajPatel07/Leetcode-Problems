56b71f5868ddb60b94ffd3d1ef08308efbb5c2cf
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-02 10:26:49 +0530

    Time: 43 ms (50.46%), Space: 13.9 MB (77.66%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,26 +1,26 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        # Create a dictionary mapping each character in the order to its index
+        # Map each character to its index in the order
         order_index = {c: i for i, c in enumerate(order)}
 
-        # Iterate over each pair of adjacent words
+        # Check each pair of adjacent words
         for i in range(len(words) - 1):
             word1 = words[i]
             word2 = words[i + 1]
 
-            # Compare characters in the words
+            # Compare characters
             for j in range(min(len(word1), len(word2))):
                 if word1[j] != word2[j]:
-                    # If the characters are different and their order is not sorted, return False
+                    # If order is not sorted, return False
                     if order_index[word1[j]] > order_index[word2[j]]:
                         return False
                     break
             else:
-                # If the end of the shorter word is reached and the length of the first word is greater, return False
+                # If end of shorter word reached and length of first word is greater, return False
                 if len(word1) > len(word2):
                     return False
 
-        # If all pairs of words are sorted, return True
+        # If all pairs are sorted, return True
         return True
 
 
