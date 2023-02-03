2d6f94c9b614940d6ae56875c9f296abc271883e
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-03 11:15:19 +0530

    Time: 61 ms (72.07%), Space: 13.9 MB (72.54%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,21 +1,28 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+        # Return input if numRows is 1
         if numRows == 1:
             return s
         
-        zigzagRows =  ["" for i in range(numRows)]
+        # Store each row of zigzag pattern
+        zigzagRows = ["" for i in range(numRows)]
         
-        row, i = 0, 1
+        # Keep track of current row and direction
+        row, direction = 0, 1
         
+        # Iterate through input string characters
         for ch in s:
+            # Add character to corresponding row
             zigzagRows[row] += ch
             
+            # Change direction at last or first row
             if row == numRows-1:
-                i = -1
-            
+                direction = -1
             if row == 0:
-                i = 1
+                direction = 1
                 
-            row += i
+            # Update row based on direction
+            row += direction
         
+        # Join and return zigzagRows as a string
         return "".join(zigzagRows)
