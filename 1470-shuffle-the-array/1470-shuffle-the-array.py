5744f4efb64b0cb7b515a80c2b51ebe0fb12fdec
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-06 10:45:46 +0530

    Time: 65 ms (60.58%), Space: 14.1 MB (86.30%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,7 +1,13 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
-        res = []
-        for i in range(n):
+        # create an empty list to store the shuffled elements
+        res = [] 
+        
+        # loop through the first half of the elements (n elements)
+        for i in range(n): 
+            # add the current element and the corresponding element from the 
+            # second half of the elements to the result list
             res.extend([nums[i], nums[i+n]])
         
+        # return the shuffled list
         return res
