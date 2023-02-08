78984ccf6b8f79306e913a12edae58e3bbccc93d
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-08 10:50:22 +0530

    Time: 135 ms (71.14%), Space: 15 MB (89.52%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,13 +1,26 @@
 class Solution:
     def jump(self, nums):
-        ans = 0
+        # Initialize the number of steps required to reach the last index
+        steps = 0
+        
         n = len(nums)
+        
+        # currEnd keeps track of the end of the current step
         currEnd = 0
+        
+        # currFarthest keeps track of the farthest point that can be reached in the current step
         currFarthest = 0
+        
+        # Iterate through the nums array
         for i in range(n-1):
-            currFarthest = max(currFarthest, i + nums[i])
-            if i == currEnd:
-                ans += 1
-                currEnd = currFarthest
-    
-        return ans
+            # Update the farthest point that can be reached
+            currFarthest = max(currFarthest, i + nums[i]) 
+            
+            # If we have reached the end of the current step, increment the number of steps
+            if i == currEnd: 
+                steps += 1
+                # Update the end of the current step to the farthest point
+                currEnd = currFarthest 
+        
+        # Return the number of steps   
+        return steps 
