131e3590cfd3e9f03e7ccdb491e81d38f7e044c9
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-08 10:46:29 +0530

    Time: 16510 ms (5.01%), Space: 15 MB (89.52%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,14 +1,24 @@
 class Solution:
-    def jump(self, nums):
-        ans = 0
+    def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        currEnd = 0
-        currFarthest = 0
-        for i in range(n-1):
-            currFarthest = max(currFarthest, i + nums[i])
-            if i == currEnd:
-                ans += 1
-                currEnd = currFarthest
-    
-        return ans
+        if n == 1:
+            return 0
         
+        # jumps[i] stores the minimum number of jumps to reach index i
+        jumps = [0] * n
+        
+        for i in range(1, n):
+            
+            jumps[i] = float('inf')
+            
+            for j in range(i):
+                
+                # check if index i can be reached from index j
+                if i <= j + nums[j] and jumps[j] != float('inf'):
+                    
+                    # update the minimum number of jumps to reach index i
+                    jumps[i] = min(jumps[i], jumps[j] + 1)
+                    break
+                    
+        # return the minimum number of jumps to reach the last index
+        return jumps[-1]
