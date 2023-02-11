815201d43efc4037296de1ca2a7aa7cd386970a8
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-11 10:55:04 +0530

    Time: 89 ms (73.46%), Space: 14.1 MB (97.21%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,9 +1,12 @@
 class Solution:
     def shortestAlternatingPaths(self, num_nodes, red_edges, blue_edges):
-        # Create a graph with 2 lists for each node, representing red and blue edges
+        # Create a graph with 2 lists for each node, 
+        # representing red and blue edges
         graph = [[[], []] for i in range(num_nodes)]
+        
         for red_start, red_end in red_edges:
             graph[red_start][0].append(red_end)
+            
         for blue_start, blue_end in blue_edges:
             graph[blue_start][1].append(blue_end)
         
@@ -20,5 +23,6 @@ class Solution:
                     result[next_node][color] = result[node][1 - color] + 1
                     bfs.append([next_node, 1 - color])
         
-        # Return the minimum distance between node 0 and all other nodes, or -1 if not possible
+        # Return the minimum distance between node 0 and all other nodes, 
+        # or -1 if not possible
         return [x if x < num_nodes * 2 else -1 for x in map(min, result)]
