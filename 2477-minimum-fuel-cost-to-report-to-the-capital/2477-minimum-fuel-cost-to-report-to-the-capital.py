0882e9d96d01c6a2a97a836232057f5139b76b91
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-12 12:25:53 +0530

    Time: 2166 ms (42.37%), Space: 163.7 MB (36.14%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -2,21 +2,38 @@ from collections import defaultdict
 
 class Solution:
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
-        G = defaultdict(list)
-        for v, w in roads:
-            G[v].append(w)
-            G[w].append(v)
-        ans = 0
-        visited = set()
-        def dfs(v):
-            nonlocal ans
-            if v in visited: return 0
-            visited.add(v)
-            res = 0
-            for w in G[v]:
-                cur = dfs(w)
-                ans += (cur + seats - 1) // seats
-                res += cur
-            return res + 1
+        # Create a dictionary to store the edges in the graph
+        graph = defaultdict(list)
+        for city_1, city_2 in roads:
+            graph[city_1].append(city_2)
+            graph[city_2].append(city_1)
+        
+        # Initialize the minimum fuel cost to 0
+        minimum_fuel_cost = 0
+        # Create a set to store the cities that have been visited
+        visited_cities = set()
+        
+        # Define a helper function to traverse the graph
+        def dfs(city):
+            nonlocal minimum_fuel_cost
+            # If the city has already been visited, return 0
+            if city in visited_cities: return 0
+            # Mark the city as visited
+            visited_cities.add(city)
+            # Initialize a variable to store the number of representatives in this city
+            representatives = 0
+            # Loop through the cities connected to this city
+            for connected_city in graph[city]:
+                # Recursively call the dfs function to count the number of representatives in the connected city
+                connected_city_representatives = dfs(connected_city)
+                # Calculate the number of cars needed to transport all the representatives in the connected city
+                minimum_fuel_cost += (connected_city_representatives + seats - 1) // seats
+                # Add the number of representatives in the connected city to the representatives variable
+                representatives += connected_city_representatives
+            # Return the number of representatives in this city, plus the representative from this city
+            return representatives + 1
+        
+        # Call the dfs function starting from city 0 (the capital city)
         dfs(0)
-        return ans
+        # Return the minimum fuel cost
+        return minimum_fuel_cost
