0c8bd93abf22f60fc4e976129c716c2ab927166a
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-21 10:13:11 +0530

    Time: 194 ms (25.24%), Space: 23.7 MB (39.30%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.py
@@ -1,17 +1,10 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        left, right = 0, len(nums) - 1
-        
+        left, right = 0, len(nums)-1
         while left < right:
-            mid = (left + right) // 2
-            
-            # if the adjacent elements are equal to the midpoint element,
-            # then the single element is on the right side of the array
-            if (mid % 2 == 1 and nums[mid - 1] == nums[mid]) or (mid % 2 == 0 and nums[mid] == nums[mid + 1]):
+            mid = int((left + right)/2)
+            if (mid % 2 == 1 and nums[mid - 1] == nums[mid]) or (mid%2 == 0 and nums[mid] == nums[mid + 1]):
                 left = mid + 1
-            # otherwise, the single element is on the left side of the array
             else:
                 right = mid
-        
-        # return the value at the left index, which is the index of the single element in the array
         return nums[left]
