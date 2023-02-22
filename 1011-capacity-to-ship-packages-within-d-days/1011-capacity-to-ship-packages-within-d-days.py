2209186c2915ae85ff50ce186065ed5ceb3249d0
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-22 08:29:35 +0530

    Time: 610 ms (33.84%), Space: 17.2 MB (31.63%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,15 +1,28 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
+        # Initialize left and right pointers.
         left, right = max(weights), sum(weights)
+
+        # Perform binary search until left >= right.
         while left < right:
+
+            # Calculate mid capacity and initialize variables.
             mid, need, cur = (left + right) // 2, 1, 0
+
+            # Check if current weight exceeds the mid capacity.
             for w in weights:
                 if cur + w > mid:
                     need += 1
                     cur = 0
+
+                # Add package weight to current weight.
                 cur += w
+
+            # If number of days needed to transport all packages is greater than given days,
+            # increase capacity by setting left = mid + 1. Otherwise, decrease capacity by setting right = mid.
             if need > days: left = mid + 1
             else: right = mid
+
+        # Return left pointer as minimum capacity needed to transport all packages within given number of days.
         return left
-        
         
