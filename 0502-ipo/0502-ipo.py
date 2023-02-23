130cd66e2724b19c9a27494353a2e3a52c2edeb5
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-23 09:19:04 +0530

    Time: 1213 ms (35.66%), Space: 38.8 MB (42.66%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -2,24 +2,23 @@ import heapq
 
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        # Create two heaps to store the projects with minimum capital and maximum profit.
-        maxProfit = []  # Heap for storing maximum profits
-        minCapital = [(c, p) for c, p in zip(capital, profits)]  # Heap for storing minimum capital needed to start a project and its profit
-        heapq.heapify(minCapital)  # Convert the capital-profits list to a heap based on minimum capital
+        # Create heaps for minimum capital and maximum profit projects.
+        maxProfit = []  
+        minCapital = [(c, p) for c, p in zip(capital, profits)]  
+        heapq.heapify(minCapital)  
         
-        # For k projects, find the project with minimum capital and add its profit to the maximum profit heap.
+        # Find k projects with minimum capital and add their profit to the max profit heap.
         for i in range(k):
             while minCapital and minCapital[0][0] <= w:
-                # Select the project with minimum capital and add its profit to the max profit heap.
                 c, p = heapq.heappop(minCapital)
-                heapq.heappush(maxProfit, -1 * p)  # Negative sign is used to create a max heap
-                
-            # Check if there are no more projects left or we don't have enough capital to start any more projects.
+                heapq.heappush(maxProfit, -1 * p)  
+            
+            # If no more projects or insufficient capital to start a project, stop.
             if not maxProfit:
                 break
                 
-            # Select the project with the maximum profit and add its profit to our current capital.
-            w += -1 * heapq.heappop(maxProfit)  # Negative sign is used to retrieve the maximum profit from max heap
-            
-        # Return the final maximized capital.
+            # Select project with maximum profit and add its profit to our capital.
+            w += -1 * heapq.heappop(maxProfit)  
+        
+        # Return final maximized capital.
         return w
