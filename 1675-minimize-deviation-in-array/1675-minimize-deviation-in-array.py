cdaf5db9109d3e8bc83d47a47c1ee31af85eba48
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-24 10:13:04 +0530

    Time: 2923 ms (32.84%), Space: 23.5 MB (26.87%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.py
@@ -1,20 +1,32 @@
 class Solution:
     def minimumDeviation(self, nums: List[int]) -> int:
+        # initialize a heap data structure to store the transformed values of the array elements
         heap = []
         for num in nums:
-            tmp = num
-            while tmp%2 == 0: tmp//=2
-            heap.append((tmp, max(num, tmp*2)))
+            # transform the number to an odd number if it is odd
+            transformed_num = num
+            while transformed_num % 2 == 0:
+                transformed_num //= 2
+            # append the tuple (transformed number, maximum limit) to the heap
+            heap.append((transformed_num, max(num, transformed_num*2)))
         
-        Max = max(i for i,j in heap)
+        # find the maximum value in the heap
+        max_num = max(i for i,j in heap)
+        # convert the list to a heap data structure
         heapify(heap)
-        ans = float("inf")
+        # initialize the minimum deviation to be a large value
+        min_deviation = float("inf")
 
+        # loop until all the elements in the heap have been processed
         while len(heap) == len(nums):
+            # pop the smallest element from the heap
             num, limit = heappop(heap)
-            ans = min(ans, Max - num)
+            # update the minimum deviation
+            min_deviation = min(min_deviation, max_num - num)
+            # if the popped element can be doubled and still be within its limit, then push it back to the heap with the doubled value
             if num < limit:
                 heappush(heap, (num*2, limit))
-                Max = max(Max, num*2)
+                max_num = max(max_num, num*2)
             
-        return ans
+        # return the minimum deviation
+        return min_deviation
