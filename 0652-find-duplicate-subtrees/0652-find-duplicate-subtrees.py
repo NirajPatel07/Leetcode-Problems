fa2efaebdc292fab3956b600b4f31a38b47fbe06
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-02-28 10:35:42 +0530

    Time: 62 ms (42.75%), Space: 27.8 MB (13.17%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
@@ -1,18 +1,33 @@
 class Solution:
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        # create a dictionary to store subtrees
         subTrees = defaultdict(list)
         
+        # define a DFS function to traverse the binary tree and find duplicate subtrees
         def dfs(node):
+            # if the node is empty, return 'null'
             if not node:
                 return 'null'
             
+            # create a string representation of the current node and its subtrees
             s = ','.join([str(node.val), dfs(node.left), dfs(node.right)])
+            
+            # if the string representation is already in subTrees, then it's a duplicate subtree
             if len(subTrees[s]) == 1:
+                # add the root node of one of the duplicate subtrees to the result list
                 res.append(node)
+                
+            # add the current node to the list of nodes with the same string representation
             subTrees[s].append(node)
+            
+            # return the string representation of the current node and its subtrees
             return s
         
+        # initialize an empty result list
         res = []
-        dfs(root)
-        return res
         
+        # traverse the binary tree and find duplicate subtrees
+        dfs(root)
+        
+        # return the root nodes of all the duplicate subtrees
+        return res
