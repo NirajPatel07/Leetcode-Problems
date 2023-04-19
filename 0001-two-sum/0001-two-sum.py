c11f8032ef06356407c6470baba1e83d032384fe
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-04-19 18:19:09 +0530

    Time: 621 ms (41.23%), Space: 15 MB (67.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Creating Dictonary for Lookup
-        lookup = {}
-        
-        for i in range(len(nums)):
-            # if target-n in lookup return n index and current index
-            if target-nums[i] in lookup:
-                return [lookup[target-nums[i]], i]
-            lookup[nums[i]] = i
-        
-            
+        for i, n in enumerate(nums):
+            if target-n in nums[i+1:]:
+                return [i, nums[i+1:].index(target-n) + i+1]
+        return -1
