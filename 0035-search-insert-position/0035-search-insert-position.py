5f5fc4f5ef899e34b2c73b75bb0b51fd9536879e
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-04-22 17:04:43 +0530

    Time: 50 ms (71.62%), Space: 14.6 MB (97.91%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,16 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+        start, end = 0, len(nums) - 1 
         
-        # Perform binary search
-        while left <= right:
-            mid = (left + right) // 2
+        while start <= end:
+            mid = (start + end) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
-                left = mid + 1
+                start = mid + 1
             else:
-                right = mid - 1
-        
-        # Target value not found, return index for insertion
-        return left
+                end = mid - 1;
+                
+        return start
