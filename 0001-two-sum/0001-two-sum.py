777031562ce9cd3b1cf76632c2153a3a37f6d7be
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-05-23 22:32:00 +0530

    Time: 63 ms (81.79%), Space: 17.6 MB (25.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,11 +2,11 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         lookup = {}
         
-        for i in range(len(nums)):
-            remaining = target - nums[i]
-            if remaining in lookup:
-                return [i, lookup[remaining]]
+        for i, n in enumerate(nums):
+            rem = target - n
+            if rem in lookup:
+                return [i, lookup[rem]]
             else:
-                lookup[nums[i]] = i
+                lookup[n] = i
         
         return -1
