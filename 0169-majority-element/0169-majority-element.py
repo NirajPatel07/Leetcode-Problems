5ea6008efecf92c880776ba18c917e346d5f5e90
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-08-06 20:44:21 +0530

    Time: 154 ms (91.64%), Space: 17.8 MB (71.27%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,12 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        # initialize the count and candidate variables
         count = 0
         candidate = None
         
-        # iterate through the array
-        for num in nums:
-            # if count is 0, update the candidate variable
+        for n in nums:
             if count == 0:
-                candidate = num
-            # increment or decrement count based on whether the current element is the candidate
-            count += (1 if num == candidate else -1)
+                candidate = n
+            
+            count += (1 if candidate == n else -1)
         
-        # return the candidate variable
         return candidate
