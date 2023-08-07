5f3f158bbb769ed993eb99137ba39867811ccb53
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-08-07 17:28:31 +0530

    Time: 41 ms (78.30%), Space: 16.4 MB (57.55%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -8,10 +8,11 @@ class Solution:
     def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
         stack = [(p, q)]
         while stack:
-            n1, n2 = stack.pop(0)
+            n1 , n2 = stack.pop()
             if not n1 and not n2: continue
             if not n1 or not n2: return n1 == n2
             if n1.val != n2.val: return False
-            stack.append((n1.right, n2.right))
             stack.append((n1.left, n2.left))
+            stack.append((n1.right, n2.right))
+        
         return True
