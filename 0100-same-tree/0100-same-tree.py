b443cc372d653962d99331f4cf3ff3fc301c4872
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-08-07 17:29:33 +0530

    Time: 35 ms (94.17%), Space: 16.5 MB (16.70%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -10,7 +10,7 @@ class Solution:
         while stack:
             n1 , n2 = stack.pop()
             if not n1 and not n2: continue
-            if not n1 or not n2: return n1 == n2
+            if not n1 or not n2: return False
             if n1.val != n2.val: return False
             stack.append((n1.left, n2.left))
             stack.append((n1.right, n2.right))
