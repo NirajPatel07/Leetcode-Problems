9a96dbf02bbdbfe9ff0d30b55ba0896bac706fc0
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-08-21 10:30:13 +0530

    Time: 38 ms (84.66%), Space: 16.4 MB (24.56%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,8 +5,6 @@ class Solution:
         while si < len(s) and ti < len(t):
             if s[si] == t[ti]:
                 si += 1
-                ti += 1
-            else:
-                ti += 1
+            ti += 1
         
         return si == len(s)
