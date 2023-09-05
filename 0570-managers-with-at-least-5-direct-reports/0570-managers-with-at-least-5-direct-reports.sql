6522109db80856eaf5b745ef81b9ffaa1ceb3953
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-09-05 20:07:24 +0530

    Time: 896 ms (12.49%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,5 +1,1 @@
-select e1.name
-from Employee e1 inner join Employee e2
-on e1.id = e2.managerId
-group by e1.id
-having count(e2.managerId) >= 5
+select name from Employee where id in (select managerId from Employee Group by managerId having count(managerId) >= 5);
