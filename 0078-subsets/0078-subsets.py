54f81fde48f9a96cb8c921934d3bf0261cba473a
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-09-30 08:48:22 +0530

    Time: 31 ms (96.80%), Space: 16.3 MB (90.32%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,15 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
         
-        def recursion(nums, curr, idx):
-            if idx >= len(nums):
-                res.append(curr.copy())
+        def find_subset(subset, i):
+            if i >= len(nums):
+                result.append(subset)
                 return
             
-            recursion(nums, curr, idx+1)
-            recursion(nums, curr + [nums[idx]], idx+1)
+            find_subset(subset, i+1)
+            find_subset(subset + [nums[i]], i+1)
         
-        recursion(nums, [], 0)
-        return res
+        result = []
+        find_subset([], 0)
+        
+        return result
