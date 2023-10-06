231e51fb775833444bb0b20909de948d873860ae
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-10-06 20:14:47 +0530

    Time: 47 ms (17.49%), Space: 16.3 MB (27.37%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.py
@@ -0,0 +1,16 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = {1: 1}
+        
+        def find_breaks(num):
+            if num in dp:
+                return dp[num]
+            
+            dp[num] = 0 if num == n else num
+            for i in range(1, num):
+                val = find_breaks(i) * find_breaks(num-i)
+                dp[num] = max(dp[num], val)
+            
+            return dp[num]
+        
+        return find_breaks(n)
