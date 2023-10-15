6062e65711e8c57e265dcfd64325bc882666ceca
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-10-15 14:27:48 +0530

    Time: 160 ms (87.22%), Space: 16.2 MB (96.67%) - LeetHub

---
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numWays(self, steps: int, arrLen: int) -> int:
+        M = 10**9 + 7
+        n = min(steps, arrLen)
+        dp = [1] + [0]*n
+        while steps:
+            prev = 0
+            for i in range(n):
+                cur = dp[i]
+                dp[i] = (cur + dp[i+1] + prev) % M
+                prev = cur
+            steps -= 1
+
+        return dp[0]
