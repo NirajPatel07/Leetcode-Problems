8e582c2008c73aa9c063a7c922e165892ad6b763
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-10-24 10:43:00 +0530

    Time: 41 ms (80.41%), Space: 16.6 MB (21.91%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
         
-        def recursion(nums, index):
-            if index >= len(nums):
+        def permutations(nums, index):
+            if index == len(nums):
                 result.append(nums.copy())
+                return
             
             for i in range(index, len(nums)):
                 nums[i], nums[index] = nums[index], nums[i]
-                recursion(nums, index+1)
+                permutations(nums, index+1)
                 nums[i], nums[index] = nums[index], nums[i]
         
-        recursion(nums, 0)
+        result = []
+        permutations(nums, 0)
         return result
