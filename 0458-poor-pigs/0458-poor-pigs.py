f06acf7ba2c9fbf9ae11564c74cbe277b956246f
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-10-29 15:34:06 +0530

    Time: 49 ms (8.80%), Space: 16.2 MB (74.40%) - LeetHub

--- 0458-poor-pigs/0458-poor-pigs.py
+++ 0458-poor-pigs/0458-poor-pigs.py
@@ -1,7 +1,8 @@
 class Solution:
-    def poorPigs(self, B, D, T):
-        tmp = log2(B) # number of pigs for one shot
-        t = ceil(T/D) # number of tests we can conduct
-
-        if t == 1: return ceil(tmp) # one shot is the answer
-        return ceil(tmp / log2(t+1)) # re-use pigs until the answer is found (# of tests)
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        pigs = 0
+        
+        while (minutesToTest / minutesToDie + 1) ** pigs < buckets:
+            pigs += 1
+        
+        return pigs
