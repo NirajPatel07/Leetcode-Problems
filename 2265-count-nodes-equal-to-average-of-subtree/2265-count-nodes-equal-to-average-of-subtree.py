55318619aecc2b4ac73dec97c99c971145038996
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-11-02 13:45:08 +0530

    Time: 57 ms (19.12%), Space: 17.6 MB (58.02%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -1,50 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
         
-        def bfs(root):
-            queue = [root]
-            total, node_count = 0, 0
+        def dfs(node):
+            if not node:
+                return (0, 0)
             
-            while queue:
-                next_queue = []
-                for root in queue:
-                    total += root.val
-                    node_count += 1
-                    
-                    if root.left:
-                        next_queue.append(root.left)
-                    if root.right:
-                        next_queue.append(root.right)
-                queue = next_queue
-            
-            return (total, node_count)
-        
-        def check(root):
-            if not root:
-                return
-            
-            subtree_sum, node_count = bfs(root)
-            average = 0
-            if subtree_sum != 0:
-                average = subtree_sum // node_count
-            
-            if root.val == average:
-                self.result += 1
-            
-            if root.left:
-                check(root.left)
-            if root.right:
-                check(root.right)
-            
-        
-        self.result = 0
-        check(root)
-        return self.result
+            left_sum, left_count = dfs(node.left)
+            right_sum, right_count = dfs(node.right)
+
+            subtree_sum = left_sum + right_sum + node.val
+            subtree_count = left_count + right_count + 1
+
+            if subtree_sum // subtree_count == node.val:
+                nonlocal result
+                result += 1
+
+            return (subtree_sum, subtree_count)
+
+        result = 0
+        dfs(root)
+        return result
             
             
