c6c479c5c6ee52a1fbe3940cfdacae589167c07a
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-11-02 13:34:49 +0530

    Time: 349 ms (9.22%), Space: 17.6 MB (28.07%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -1,19 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
-        hashmap={}
-        def rec(node):
-            if node==None:
-                return (0,0)
-            n1,s1=rec(node.left)
-            n2,s2=rec(node.right)
-            n3=1+n1+n2
-            s3=s1+s2+node.val
-            hashmap[node]=(n3,s3)
-            return (n3,s3)
-        rec(root)
-        c=0
-        for i in hashmap:
-            cur=hashmap[i]
-            if i.val==cur[1]//cur[0]:
-                c+=1
-        return c
+        
+        def bfs(root):
+            queue = [root]
+            total, node_count = 0, 0
+            
+            while queue:
+                next_queue = []
+                for root in queue:
+                    total += root.val
+                    node_count += 1
+                    
+                    if root.left:
+                        next_queue.append(root.left)
+                    if root.right:
+                        next_queue.append(root.right)
+                queue = next_queue
+            
+            return (total, node_count)
+        
+        def check(root):
+            if not root:
+                return
+            
+            subtree_sum, node_count = bfs(root)
+            average = 0
+            if subtree_sum != 0:
+                average = subtree_sum // node_count
+            
+            if root.val == average:
+                self.result += 1
+            
+            if root.left:
+                check(root.left)
+            if root.right:
+                check(root.right)
+            
+        
+        self.result = 0
+        check(root)
+        return self.result
+            
+            
