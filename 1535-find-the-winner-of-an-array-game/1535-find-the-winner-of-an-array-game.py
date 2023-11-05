be1b8b8182f5228f852331f4c2022f968376cd2a
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-11-05 19:40:08 +0530

    Time: 528 ms (64.67%), Space: 30.1 MB (51.09%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.py
@@ -2,7 +2,6 @@ class Solution:
     def getWinner(self, arr: List[int], k: int) -> int:
         winner = arr[0]
         count = 0
-        max_n = max(arr)
         
         for i in range(1, len(arr)):
             if winner > arr[i]:
@@ -10,9 +9,6 @@ class Solution:
             else:
                 winner = arr[i]
                 count = 1
-            
-            if winner == max_n:
-                return winner
             
             if count == k:
                 break
