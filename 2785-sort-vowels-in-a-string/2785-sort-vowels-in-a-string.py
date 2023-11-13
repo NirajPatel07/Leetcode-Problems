18d78c4a2588e80646645bcac9793d9ca3441c04
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-11-13 09:59:05 +0530

    Time: 121 ms (94.53%), Space: 24 MB (12.55%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -1,31 +1,16 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+        s = list(s)
+        stackVowel = []
+        stackIndex = []
+        vowels = {'a','e','i','o','u','A','E','I','O','U'}
+        for i in range(len(s)):
+            if s[i] in vowels:
+                stackVowel.append(s[i])
+                stackIndex.append(i)
         
-        s_vowels = []
-        for c in s:
-            if c not in vowels:
-                continue
-            s_vowels.append(c)
-            
-        s_vowels.sort()
-        result = ""
-        
-        idx = 0
-        for c in s:
-            if c not in vowels:
-                result += c
-            else:
-                result += s_vowels[idx]
-                idx += 1
-                
-        if idx < len(s_vowels):
-            while idx < len(s_vowels):
-                result += s_vowels[idx]
-                idx += 1
-                
-        return result
-        
-        
-                
-        
+        stackVowel.sort()
+
+        for i in range(len(stackVowel)):
+            s[stackIndex[i]] = stackVowel[i]
+        return "".join(s)
