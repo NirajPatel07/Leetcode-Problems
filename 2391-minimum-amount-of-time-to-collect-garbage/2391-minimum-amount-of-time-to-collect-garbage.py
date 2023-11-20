4b5e863005ddcf4e1d8d43bbd71ea69363330d26
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-11-20 18:54:08 +0530

    Time: 1017 ms (23.47%), Space: 39.4 MB (34.21%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -11,10 +11,13 @@ class Solution(object):
             "M": 0,
         }
         total_minutes = 0
+        
         for garbage_house, travel_to_house in reversed(list(zip(garbage, [0] + travel))):
             for garbage_name in garbage_house:
                 count_garbage_by_name[garbage_name] += 1
+                
             if travel_to_house > 0:
                 total_minutes += travel_to_house * len([c for c in count_garbage_by_name.values() if c > 0])
+                
         total_minutes += sum(count_garbage_by_name.values())
         return total_minutes
