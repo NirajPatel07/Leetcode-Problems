7f862cbb1e8f743cba0f59b24331d01b05ced713
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-11-30 17:16:07 +0530

    Time: 50 ms (73.06%), Space: 41.8 MB (90.13%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    if (str1.length < str2.length){
+        [str1, str2] = [str2, str1];
+    }
+    
+    if (str1 == str2){
+        return str1;
+    }
+    
+    if (str1.slice(0, str2.length) != str2){
+        return "";
+    }
+    
+    return gcdOfStrings(str1.slice(str2.length, str1.length), str2)
+};
