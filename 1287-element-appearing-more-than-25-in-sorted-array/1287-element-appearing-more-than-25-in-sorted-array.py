8d76a5b04f7652a8b18d3ffc57cac2f7919d55ab
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2023-12-11 11:55:29 +0530

    Time: 96 ms (40.00%), Space: 17.8 MB (48.07%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
@@ -1,17 +1,25 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        res = arr[0]
-        count = 1
-        percentage = len(arr)//4
+        N = len(arr)
         
-        for num in arr[1:]:
-            if num == res:
-                count += 1
-            else:
-                res = num
-                count = 1
+        def binary_search(arr: List[int], left: int, right:int, target: int) -> int:
+            while left <= right:
+                mid = left + (right-left)//2
+                if arr[mid] > target:
+                    right = mid - 1
+                elif arr[mid] < target:
+                    left = mid + 1
+                elif arr[mid] == target:
+                    left = mid + 1
+                    
+            return right
+        
+        i = 0
+        while i < N:
+            target = arr[i]
+            index = binary_search(arr, i, N-1, target)
             
-            if count > percentage:
-                return num
-        
-        return res
+            if index - i + 1 > 0.25*N:
+                return arr[i]
+            
+            i = index + 1
