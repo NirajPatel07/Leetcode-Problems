47a8fbf5476a33551aa69ab45464715cde1a43e3
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2024-01-03 18:34:09 +0530

    Time: 101 ms (35.29%), Space: 52.1 MB (43.53%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.js
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.js
@@ -3,17 +3,16 @@
  * @return {number}
  */
 var numberOfBeams = function(bank) {
-    let prev = bank[0].split('1').length - 1;
-    console.log(bank[0].split('1'));
-        let res = 0;
-
-        for (let i = 1; i < bank.length; i++) {
-            let curr = bank[i].split('1').length - 1;
-            if (curr) {
-                res += prev * curr;
-                prev = curr;
-            }
+    let prev = bank[0].split("").filter((char) => char == "1").length;
+    let res = 0;
+    
+    for (let i = 1; i < bank.length; i++){
+        let curr = bank[i].split("").filter((char) => char == "1").length;
+        if (curr){
+            res += (prev * curr);
+            prev = curr;
         }
-
-        return res;
+    }
+    
+    return res;
 };
