d73c68b7031692e5665d6f38d75a2726de5d8a4a
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2024-01-05 18:26:49 +0530

    Time: 131 ms (63.01%), Space: 44.1 MB (37.67%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+    const lis = Array(nums.length).fill(1);
+    
+    for (let i = nums.length - 1; i >= 0; i--){
+        for (let j = i + 1; j < nums.length; j++){
+            if (nums[i] < nums[j]){
+                lis[i] = Math.max(lis[i], 1 + lis[j]);
+            }
+        }
+    }
+    
+    return Math.max(...lis);
+};
