a49feb5a32eef3af0f66ee0722554cbcfd3f47da
Niraj Patel <66070865+NirajPatel07@users.noreply.github.com>
2024-01-06 20:01:46 +0530

    Time: 690 ms (47.03%), Space: 29.6 MB (77.49%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,22 +1,31 @@
-from bisect import bisect_right
+from typing import List
+
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        arr = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])
-        n = len(arr)
-        dp = [0] * n    # dp[i]: Maimum profit with 0~i-th items taken into consideration
+        jobs = sorted(zip(startTime, endTime, profit), key=lambda x: x[1])  
+        n = len(jobs)
+        
+        dp = [0] * n  
         
         for i in range(n):
-            s, e, p = arr[i]
-            idx = bisect_right(arr, s, key=lambda x: x[1])  # Find the 1st item whose end_time is <= my start_time
+            # Binary search to find the latest job that doesn't overlap
+            prev_job_index = self.binarySearch(jobs, i)
             
-            # 1. `take_i` means the maximum profit if i-th is one of the taken-job
-            if idx == 0:
-                take_i = p
+            # Maximum profit without including the current job
+            without_current = dp[i - 1] if i > 0 else 0
+            dp[i] = max(without_current, dp[prev_job_index] + jobs[i][2])
+        
+        return dp[-1]
+    
+    def binarySearch(self, jobs, current_index):
+        low, high = 0, current_index - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if jobs[mid][1] <= jobs[current_index][0]:
+                if jobs[mid + 1][1] <= jobs[current_index][0]:
+                    low = mid + 1
+                else:
+                    return mid
             else:
-                take_i = dp[idx - 1] + p
-                
-            # 2. Stick to the "definition" of dp[i], so we should apply knock-down strategy  -- max() function -- to dp[i]!
-            dp[i] = take_i
-            if i > 0:
-                dp[i] = max(dp[i], dp[i - 1])
-        return max(dp)
+                high = mid - 1
+        return -1
